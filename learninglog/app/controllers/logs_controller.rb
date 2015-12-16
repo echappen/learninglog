385415931c581794dbef3de584a966561f99cafa
echappen <elenichappen@gmail.com>
2015-12-16 07:58:18 -0600

    added form for new logs

--- learninglog/app/controllers/logs_controller.rb
+++ learninglog/app/controllers/logs_controller.rb
@@ -1,8 +1,15 @@
 class LogsController < ApplicationController
 
-
   def index
     @user = User.find(params[:user_id].to_i)
     @logs = @user.logs
+  end
+
+  def new
+    @logs = Log.new
+  end
+
+  def create
+
   end
 end
